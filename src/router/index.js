9b943394b7ed68808be9b32a919a60a89e151c1f
1903247335 <1903247335@qq.com>
2022-11-10 15:58:48 +0800

    更新

---
+++ src/router/index.js
@@ -0,0 +1,6 @@
+import {createMemoryHistory,createRouter} from "vue-router";
+const route=createRouter({
+    history:createMemoryHistory(),
+    routes:[{component:'',path}]
+})
+export default route
